9bc1f71301d9bf445df062eaa83326eac1a322d1
harish-raghu <coolharishraghu@gmail.com>
2018-01-09 09:37:59 +0000

    Create Variables.vb

---
+++ Help and Solutions/Variables.vb
@@ -0,0 +1,34 @@
+Module Module1
+
+    Sub Main()
+        'This is what we did in the lesson
+        Dim MyFirstString As String
+        MyFirstString = "hello"
+        Console.WriteLine(MyFirstString)
+        Console.ReadLine()
+
+        'These are the challenges that I set you
+
+        'Challenge 1 - Make an integer with a value of 32. Print it to the console.
+        Dim MyInteger As Integer
+        MyInteger = 32
+        Console.WriteLine(MyInteger)
+        Console.ReadLine()
+
+        'Challenge 2 - Make your name a string, and print it to the console.
+        Dim MyName As String
+        MyName = "Harish Raghu"
+        Console.WriteLine(MyName)
+        Console.ReadLine()
+        
+        'Challenge 3 - Create two strings. Make the first string your first name, and the second one your last name. Print both of them out.
+        Dim MyFirstName As String
+        Dim MySecondName As String
+        MyFirstName = "Harish"
+        MySecondName = "Raghu"
+        Console.WriteLine(MyFirstName)
+        Console.WriteLine(MySecondName)
+        Console.ReadLine()
+    End Sub
+
+End Module
